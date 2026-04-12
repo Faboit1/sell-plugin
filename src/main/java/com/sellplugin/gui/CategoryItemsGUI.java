d7ed507d219dd8320653c7c96b851a995e3dc04e
Faboit1 <omarassayed3@gmail.com>
2026-04-12 19:55:06 +0200

    Updated CategoryItemsGUI.java with new implementation

--- src/main/java/com/sellplugin/gui/CategoryItemsGUI.java
+++ src/main/java/com/sellplugin/gui/CategoryItemsGUI.java
@@ -3,7 +3,7 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.event.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
 
